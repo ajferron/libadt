6cc61f16ef8667b279cf98db6739bdf11d2c9365
AJ Ferron <alvn.frrn@gmail.com>
2019-02-26 22:17:16 -0500

    Create stack function prototypes

--- include/libadt.h
+++ include/libadt.h
@@ -17,16 +17,22 @@ struct adt {
 
 typedef struct adt adt, *list, *stack, *que;
 
-/* Abstract Function Prototypes */
-
+// Return a pointer to an array with all items in data structure
 void *toArray(adt *);
 
+// Return 1 if length is 0, and 0 if length is any other value
 int isEmpty(adt *);
 
+// Return the number of items in data structure
 int length(adt *);
 
+// Free all memory associated with data structure. Use List()/Stack()/Que() to re-initialize
 void clear(adt *);
 
+/* Return the head of the data structure
+ * List: returns first item added
+ * Stack: returns last item added
+ * Que: returns first item added */
 void *peek(adt *);
 
 #endif
--- include/stack.h
+++ include/stack.h
@@ -3,10 +3,13 @@
 #ifndef _STACK_H_
 #define _STACK_H_
 
+// Create memory allocation for stack (struct adt)
 stack Stack(void);
 
+// Add item to the top of stack
 void push(stack, void *);
 
-void *peek(stack);
+// Remove and return the last item added
+void* pop(stack);
 
 #endif
