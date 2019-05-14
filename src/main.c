45e7b5e2b09b09164a73e373d85710615a81d5a9
AJ Ferron <alvn.frrn@gmail.com>
2019-05-13 21:58:31 -0400

    Add testing for the multiset data type

--- src/main.c
+++ src/main.c
@@ -8,27 +8,25 @@
 #define TEST_LIST 1
 #define TEST_STACK 1
 #define TEST_QUEUE 1
-#define TEST_SET 0
+#define TEST_MSET 1
 #define N 10
 #define C 'j'
 
 
 int main(void) {
-    list lst;
-    stack stk;
-    queue q;
 
     int i, j;
 
 #if TEST_LIST
 
+    list lst;
     int c, *l_arr;
     char *l_str;
 
+    printf("\n------------------------| TEST LIST |------------------------\n\n");
+
     lst = create_list();
     c = 'a';
-
-    printf("\n------------------------| TEST LIST |------------------------\n\n");
 
     for (i = 0; i < N; i++)
         list_append(lst, i + 10);
@@ -91,16 +89,16 @@ int main(void) {
 
 #if TEST_STACK
 
-    stk = create_stack();
+    stack stk;
 
-    printf("\n-----------------------| TEST STACK |------------------------\n");
+    printf("\n-----------------------| TEST STACK |------------------------\n\n");
+
+    stk = create_stack();
 
     for (i = 0; i < N; i++) { 
         stack_push(stk, i);
-        printf("\nstack_push(stk, %d) | stack_peek(stk) = %d ", i, stack_peek(stk));
+        printf("stack_push(stk, %d) | stack_peek(stk) = %d\n", i, stack_peek(stk));
     }
-
-    printf("\n\n");
 
     for (i = 0; i < N; i++)
         printf("\nstack_pop(stk) = %d", stack_pop(stk));
@@ -114,16 +112,17 @@ int main(void) {
 
 #if TEST_QUEUE
 
-    q = create_queue();
+    queue q;
 
-    printf("\n------------------------| TEST QUEUE |-------------------------\n");
+    printf("\n------------------------| TEST QUEUE |-------------------------\n\n");
+
+    q = create_queue();
 
     for (i = 0; i < N; i++) { 
         enqueue(q, i);
-        printf("\nenqueue(q, %d) | queue_head(q) = %d | queue_tail(q) = %d", i, queue_head(q), queue_tail(q));
+        printf("enqueue(q, %d) | queue_head(q) = %d | queue_tail(q) = %d\n", i, queue_head(q), queue_tail(q));
     }
 
-    printf("\n\n");
 
     while (!queue_empty(q))
         printf("\ndequeue(q) = %d", dequeue(q));
@@ -131,6 +130,55 @@ int main(void) {
     printf("\n\n");
 
     free_queue(q);
+
+#endif
+
+
+#if TEST_MSET
+
+    multiset ms1, ms2, ms_temp;
+
+    printf("\n-------------------------| TEST SET |-------------------------\n");
+
+    ms1 = create_mset();
+
+    for (i = 0; i < N; i++)
+        mset_add(ms1, i);
+
+    printf("\ncardinality(ms1) = %d", cardinality(ms1));
+
+    ms2 = interval(0, N);
+    printf("\ncardinality(ms2) = %d\n", cardinality(ms2));
+
+    for (i = 0; i <= N; i++)
+        printf("\nmset_contains(ms1, %d) = %d", i, mset_contains(ms1, i));
+
+    ms_temp = mset_union(ms1, ms2);
+    printf("\n\ncardinality(mset_union(ms1, ms2)) = %d\n", cardinality(ms_temp));
+
+    for (i = 0; i <= N; i++)
+        printf("\nmultiplicity(msu, %d) = %d", i, multiplicity(ms_temp, i));
+
+    free_mset(ms_temp);
+    ms_temp = mset_intersection(ms1, ms2);
+    printf("\n\ncardinality(mset_intersection(ms1, ms2)) = %d", cardinality(ms_temp));
+
+    free_mset(ms_temp);
+    ms_temp = mset_difference(ms2, ms1);
+    printf("\n\ncardinality(mset_difference(ms2, ms1)) = %d", cardinality(ms_temp));
+
+    printf("\n\nequivalent(ms2, ms1) = %d\n", equivalent(ms2, ms1));
+
+    for (i = 0; i <= N; i++) {
+        mset_remove(ms2, i);
+        printf("\nmset_remove(ms2, %d) | mset_contains(ms2, %d) = %d | cardinality(ms2) = %d", i, i, mset_contains(ms2, i), cardinality(ms2));
+    }
+
+    printf("\n\n");
+
+    free_mset(ms1);
+    free_mset(ms2);
+    free_mset(ms_temp);
 
 #endif
 
